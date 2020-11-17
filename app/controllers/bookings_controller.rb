4214d9fc1f779f11c1c923c3c1c176b6f915f736
Jedrzej Lagodzinski <jedrzejszymon@gmail.com>
2020-11-17 14:09:10 +0100

    added new and create for booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,32 @@
 class BookingsController < ApplicationController
   def index
+    @bookings = Booking.all
   end
 
   def show
   end
 
   def new
+    @booking = Booking.new
+    @pet = Pet.find(params[:pet_id])
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @pet = Pet.find(params[:pet_id])
+    @booking.pet = @pet
+
+    if @booking.save
+      redirect_to pet_path(@booking)
+    else
+      @booking.save
+      render pet_booking_path
+    end
   end
+
+  private
+
+    def booking_params
+      params.require(:booking).permit(:start_date, :end_date, :pet_id, :user_id)
+    end
 end
