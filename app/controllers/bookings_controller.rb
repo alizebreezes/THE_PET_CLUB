f017dca1ee7b61f38909ce78da4b38c50e71d71b
Jedrzej Lagodzinski <jedrzejszymon@gmail.com>
2020-11-17 16:15:19 +0100

    changing last to current_user

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -14,7 +14,7 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(booking_params)
     @pet = Pet.find(params[:pet_id])
-    @booking.user = User.last
+    @booking.user = current_user
     @booking.pet = @pet
 
     if @booking.save!
