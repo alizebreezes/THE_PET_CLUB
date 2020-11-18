7efaf34a7e940c5e75cbf9bf660b080f91c588c8
Antje Heiermann <antje.heiermann@web.de>
2020-11-18 15:43:14 +0100

    Seed file with images

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,10 @@ Pet.destroy_all
 User.destroy_all
 
 
-martin = User.create!(name: "martin", location: "stockholm", bio: "bla bla", email: "martin@m.com", password: "123456")
-j = User.create!(name: "j", location: "copenhagen", bio: "bla bla bla", email: "j@jensray.com", password: "123456")
+martin = User.create!(name: "martin", location: "stockholm", bio: "bla bla", email: "martin@martin.com", password: "123456")
+j = User.create!(name: "j", location: "copenhagen", bio: "bla bla bla", email: "j@j.com", password: "123456")
 alize = User.create!(name: "alize", location: "stockholm", bio: "bla bla", email: "alize@alize.com", password: "123456")
-antje = User.create!(name: "antje", location: "stockholm", bio: "bla bla bla", email: "antje@frauantje.com", password: "123456")
+antje = User.create!(name: "antje", location: "stockholm", bio: "bla bla bla", email: "antje@antje.com", password: "123456")
 michael= User.create!(name: "michael", location: "stockholm", bio: "bla bla", email: "michael@michael.com", password: "123456")
 samad = User.create!(name: "samad", location: "copenhagen", bio: "bla bla bla", email: "samad@samad.com", password: "123456")
 tom = User.create!(name: "tom", location: "stockholm", bio: "bla bla", email: "tom@tom.com", password: "123456")
@@ -27,54 +27,54 @@ bruno = User.create!(name: "bruno", location: "lisbon", bio: "bla bla bla", emai
 
 
 file = File.open("app/assets/images/terry.jpg")
-pet = Pet.new(name: "terry", species: "dog", age: 15, description: "bites", user: martin)
-pet.photo.attach(io: file, filename: 'terry.jpg', content_type: 'image/jpg')
-pet.save!
+terry = Pet.new(name: "terry", species: "dog", age: 15, description: "bites", user: martin)
+terry.photo.attach(io: file, filename: 'terry.jpg', content_type: 'image/jpg')
+terry.save!
 
-# file = File.open(image_path 'cat.jpg')
-# pet = Pet.new(name: "zorro", species: "cat", age: 5, description: "meows", user: j)
-# pet.photo.attach(io: file, filename: 'cat.jpg', content_type: 'image/jpg')
-# pet.save!
-
-
-# file = File.open(image_path 'dog-running.jpg')
-# pet = Pet.new(name: "jessy", species: "dog", age: 15, description: "Sweet dog. Doesn't like German shepherd dogs.", user: martin)
-# pet.photo.attach(io: file, filename: 'dog-running.jpg', content_type: 'image/jpg')
-# pet.save!
-
-# file = File.open(image_path 'cat2.jpg')
-# pet = Pet.new(name: "brutus", species: "cat", age: 5, description: "meows", user: antje)
-# pet.photo.attach(io: file, filename: 'cat2.jpg', content_type: 'image/jpg')
-# pet.save!
-
-# file = File.open(image_path 'dogs.jpg')
-# pet = Pet.new(name: "khan", species: "dog", age: 15, description: "Lazy dogs. Only needs a short round", user: martin)
-# pet.photo.attach(io: file, filename: 'dogs.jpg', content_type: 'image/jpg')
-# pet.save!
+file = File.open("app/assets/images/cat.jpg")
+zorro = Pet.new(name: "zorro", species: "cat", age: 5, description: "meows", user: j)
+zorro.photo.attach(io: file, filename: 'cat.jpg', content_type: 'image/jpg')
+zorro.save!
 
 
-# file = File.open(image_path 'cat2.jpg')
-# pet = Pet.new(name: "doro", species: "cat", age: 5, description: "meows", user: j)
-# pet.photo.attach(io: file, filename: 'cat2.jpg', content_type: 'image/jpg')
-# pet.save!
+file = File.open("app/assets/images/dog-running.jpg")
+jessy = Pet.new(name: "jessy", species: "dog", age: 15, description: "Sweet dog. Doesn't like German shepherd dogs.", user: martin)
+jessy.photo.attach(io: file, filename: 'dog-running.jpg', content_type: 'image/jpg')
+jessy.save!
 
-# file = File.open(image_path 'dogs2.jpg')
-# pet = Pet.new(name: "hasso", species: "dog", age: 15, description: "Loves kids. No danger to anyone.", user: martin)
-# pet.photo.attach(io: file, filename: 'dogs2.jpg', content_type: 'image/jpg')
-# pet.save!
+file = File.open("app/assets/images/cat2.jpg")
+brutus = Pet.new(name: "brutus", species: "cat", age: 5, description: "meows", user: antje)
+brutus.photo.attach(io: file, filename: 'cat2.jpg', content_type: 'image/jpg')
+brutus.save!
 
-# file = File.open(image_path 'cat3.jpg')
-# pet = Pet.new(name: "kitty", species: "cat", age: 5, description: "meows", user: j)
-# pet.photo.attach(io: file, filename: 'cat3.jpg', content_type: 'image/jpg')
-# pet.save!
+file = File.open("app/assets/images/dogs.jpg")
+khan = Pet.new(name: "khan", species: "dog", age: 15, description: "Lazy dogs. Only needs a short round", user: martin)
+khan.photo.attach(io: file, filename: 'dogs.jpg', content_type: 'image/jpg')
+khan.save!
+
+
+file = File.open("app/assets/images/cat2.jpg")
+doro = Pet.new(name: "doro", species: "cat", age: 5, description: "meows", user: j)
+doro.photo.attach(io: file, filename: 'cat2.jpg', content_type: 'image/jpg')
+doro.save!
+
+file = File.open("app/assets/images/dogs2.jpg")
+hasso = Pet.new(name: "hasso", species: "dog", age: 15, description: "Loves kids. No danger to anyone.", user: martin)
+hasso.photo.attach(io: file, filename: 'dogs2.jpg', content_type: 'image/jpg')
+hasso.save!
+
+file = File.open("app/assets/images/cat3.jpeg")
+kitty = Pet.new(name: "kitty", species: "cat", age: 5, description: "meows", user: j)
+kitty.photo.attach(io: file, filename: 'cat3.jpeg', content_type: 'image/jpg')
+kitty.save!
 
 
 
-Booking.create(start_date: "2020, 11, 9", end_date: "2020, 12, 04", user: martin, pet: fido)
+Booking.create(start_date: "2020, 11, 9", end_date: "2020, 12, 04", user: martin, pet: doro)
 Booking.create(start_date: "2020, 12, 15", end_date: "2020, 12, 19", user: j, pet: brutus)
 Booking.create(start_date: "2020, 11, 19", end_date: "2020, 11, 19", user: antje, pet: jessy)
 Booking.create(start_date: "2020, 12, 15", end_date: "2020, 12, 19", user: alize, pet: zorro)
-Booking.create(start_date: "2020, 11, 9", end_date: "2020, 12, 04", user: martin, pet: fido)
+Booking.create(start_date: "2020, 11, 9", end_date: "2020, 12, 04", user: martin, pet: kitty)
 Booking.create(start_date: "2020, 12, 17", end_date: "2020, 12, 18", user: j, pet: terry)
 
 
