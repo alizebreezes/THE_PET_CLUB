8dcf7618ba148590f37a2fe66ea8e0e8b40d3dc0
Jedrzej Lagodzinski <jedrzejszymon@gmail.com>
2020-11-20 15:35:17 +0100

    slider fixed

--- app/javascript/packs/rangeInput.js
+++ app/javascript/packs/rangeInput.js
@@ -1,21 +1,15 @@
-const rangeInput = document.querySelector(".pet-range-input");
-const rangeValue = document.querySelector("#range-field-value");
-
 const updateRange = (e) => {
+  const rangeValue = document.querySelector("#range-field-value");
   const value = e.target.value;
-  console.log(value);
   rangeValue.innerText = `pets up to ${value} years old`;
 }
 
-
-
-
-
 const initRange = () => {
+  const rangeInput = document.querySelector(".pet-range-input");
+
   if (rangeInput) {
     rangeInput.addEventListener("input", updateRange);
   }
 }
-
 
 export default initRange;
