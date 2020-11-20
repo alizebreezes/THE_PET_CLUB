7b16b5d665904fa148a16c253db47d9c0ba55710
Jedrzej Lagodzinski <jedrzejszymon@gmail.com>
2020-11-20 14:10:52 +0100

    seeds for locations and pets

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -12,7 +12,6 @@ const fitMapToMarkers = (map, markers) => {
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
-    console.log(popup)
     new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
       .setPopup(popup) // add this
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -5,7 +5,7 @@ class Pet < ApplicationRecord
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
   validates :name, presence: true
-  validates :species, presence: true, inclusion: { in: ["cat", "dog", "turtle", "rabit", "hamster", "mouse", "bird", "fish", "other"]}
+  validates :species, presence: true, inclusion: { in: ["cat", "dog", "turtle", "rabbit", "hamster", "mouse", "bird", "fish", "other"]}
   validates :age, presence: true, inclusion: 0..100
   validates :description, presence: true
 end
